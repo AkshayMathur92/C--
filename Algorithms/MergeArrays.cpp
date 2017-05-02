78ae9a82e12513d2b99a5f077e78786d569703e2
Akshay Mathur <avi.mathur.engg@gmail.com>
2017-05-02 15:57:26 +0530

    Merging arrays variadic templates with a bug

---
+++ Algorithms/MergeArrays.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+#include <numeric>
+
+template <typename T>
+T  merge_arrays(T &v1 , T &v2){
+    T  res;
+    auto iv1 = v1.begin();
+    auto iv2 = v2.begin();
+    while(iv1 != v1.end() && iv2 != v2.end()){
+        if(*iv1 < *iv2){
+            res.push_back(*iv1);
+            iv1++;
+        }
+        else{
+            res.push_back(*iv2);
+            iv2++;
+        }
+    }
+    if(iv1 != v1.end())
+        std::copy(iv1, v1.end(),back_inserter(res));
+    else
+        std::copy(iv2, v2.end(),back_inserter(res));
+    return res;
+}
+template <typename T, typename... Tail>
+T merge_arrays(T &v1 , T &v2,Tail... tail){
+	auto res1 = merge_arrays(v1,v2);
+	auto res2 = merge_arrays(tail...);
+	return merge_arrays(res1, res2);
+}
+int main(){
+    std::vector<int> v1 = {1,4,6};
+    std::vector<int> v2 = {2,7,9};
+    std::vector<int> v3 = {3,5,8};
+    std::vector<int> v4 = {10};
+    std::vector<int> v5 = {11};
+    for(auto i : merge_arrays(v1,v2,v3,v4)){
+    	std::cout << i << " ";
+    }
+}
