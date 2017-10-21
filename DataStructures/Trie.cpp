d4ae9c31901b19121569d9edb1e4432c0da463d1
Akshay Mathur <avi.mathur.engg@gmail.com>
2017-10-21 15:28:29 +0530

    Adding remove operation in trie.

--- DataStructures/Trie.cpp
+++ DataStructures/Trie.cpp
@@ -9,12 +9,13 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Trie{
     struct TrieNode{
         struct ch{
-            array<TrieNode*, 26> children{};
+            TrieNode* children[26];
             TrieNode* operator[](int i)const {
                 auto temp = (tolower(i) - 'a') % 26;
                 return children[temp];
@@ -22,6 +23,11 @@ class Trie{
             TrieNode*& operator[](int i){
                 auto temp = (tolower(i) - 'a') % 26;
                 return children[temp];
+            }
+            ch(){
+                for(int i = 0; i < 26; i++){
+                        children[i] = nullptr;
+                }
             }
             ~ch(){
                 for(int i = 0; i < 26; i++){
@@ -35,6 +41,42 @@ class Trie{
         TrieNode():isEnd(false){}
     };
     TrieNode *root;
+    
+    bool isLeaf(TrieNode* itr){
+        for(int i = 'a'; i <= 'z'; i ++){
+            if(itr ->children[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool remove(TrieNode * itr, string s, int index){
+        if(itr == nullptr)
+            return false;
+        if(index == s.length() - 1){
+            if(isLeaf(itr)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if(remove(itr -> children[s[index + 1]], s, index + 1)){
+                delete itr -> children[s[index + 1]];
+                itr -> children[s[index + 1]] = nullptr;
+                if(isLeaf(itr)){
+                    itr  -> isEnd =true;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            return false;
+        }
+    }
+    
 public:
     Trie(){
         root = new TrieNode();
@@ -54,7 +96,7 @@ public:
         itr -> isEnd = true;
     }
     
-    bool search(string s){
+    bool search(string s) const{
         TrieNode *itr = root;
         for(int i = 0; i < s.length(); i++){
             if(!itr -> children[s[i]])
@@ -65,6 +107,15 @@ public:
         }
         return false;
     }
+    
+    void remove(string s)
+    {
+        if( s.size() > 0 )
+        {
+            remove(root -> children[s[0]], s, 0);
+        }
+    }
+    
 };
 
 int main(){
@@ -74,5 +125,21 @@ int main(){
         t.add("World");
         cout << t.search("Hello") << endl;
         cout << t.search("dog") << endl;
+        t.remove("Hello");
+        cout << t.search("Hello") << endl;
+        t.add("she");
+        t.add("sells");
+        t.add("sea");
+        t.add("shore");
+        cout << t.search("she") << endl;
+        cout << t.search("sells") << endl;
+        cout << t.search("sea") << endl;
+        cout << t.search("shore") << endl;
+        
+        t.remove("sea");
+        cout << t.search("she") << endl;
+        cout << t.search("sells") << endl;
+        cout << t.search("sea") << endl;
+        cout << t.search("shore") << endl;
     }
 }
